850bae5eb834810ecd87c8d17a325076228f0780
syphrose <syphrose.wabulwenyi@student.moringaschool.com>
2022-06-18 23:36:30 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+
+    let form = document.querySelector('form')
+    form.addEventListener('submit', (event) =>{
+      event.preventDefault()
+      addTodo(event.target['new-task-description'].value)
+      form.reset()
+    })
+  });
+  function addTodo(input){
+    let li = document.createElement('li')
+    let btn = document.createElement('button')
+    btn.textContent = 'x'
+    btn.addEventListener('click', (event)=> event.target.parentNode.remove())
+    li.textContent = `${input}  `
+    document.querySelector('#list').appendChild(li)
+    li.appendChild(btn)
+  }
+  
